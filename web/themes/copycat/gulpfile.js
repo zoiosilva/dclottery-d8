50d54570c0daf95f41ce4ed70efcce982f215fce
Chaz Chumley <chaz.chumley@gmail.com>
2019-07-02 09:46:49 -0400

    update copycat

--- web/themes/copycat/gulpfile.js
+++ web/themes/copycat/gulpfile.js
@@ -1,13 +1,21 @@
 'use strict';
 
-const { watch, dest, src, series, parallel } = require('gulp');
+const { dest, lastRun, parallel, series, src, watch } = require('gulp');
 const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
 const sassGlob = require('gulp-sass-glob');
+const stylelint = require('gulp-stylelint');
 const postcss = require('gulp-postcss');
-const del = require('del');
 const config = require('./patternlab-config.json');
 const patternlab = require('@pattern-lab/core')(config);
+
+function lintStyles() {
+  return src('**/*.scss', { cwd: './source', since: lastRun(lintStyles) })
+    .pipe(stylelint({
+      failAfterError: true,
+      reporters: [{ formatter: 'string', console: true }]
+    }));
+}
 
 function buildStyles() {
   return src('*.scss', { cwd: './source' })
@@ -29,12 +37,6 @@ function buildStyles() {
     .pipe(dest('css'));
 }
 
-function cleanPatternlab() {
-  return del([
-    'pattern-lab/public',
-  ]);
-}
-
 function buildPatternlab() {
   return patternlab.build({cleanPublic: true, watch: false});
 }
@@ -43,20 +45,20 @@ function fileWatch() {
   watch(
     ['source/**/*.scss', 'images/*.svg'],
     { usePolling: true, interval: 1500 },
-    buildStyles
+    series(
+      lintStyles,
+      buildStyles
+    ),
   );
   watch(
     'source/**/*.{twig,json,yaml,yml,md}',
     { usePolling: true, interval: 1500 },
-    series(
-      cleanPatternlab,
-      buildPatternlab
-    ),
+    buildPatternlab
   );
 }
 
-const copycatBuildPatternlab = exports.copycatBuildPatternlab = series(cleanPatternlab, buildPatternlab);
-const copycatBuildStyles = exports.copycatBuildStyles = buildStyles;
+const copycatBuildPatternlab = exports.copycatBuildPatternlab = buildPatternlab;
+const copycatBuildStyles = exports.copycatBuildStyles = series(lintStyles, buildStyles);
 const copycatBuild = exports.copycatBuild = parallel(copycatBuildStyles, copycatBuildPatternlab);
 const copycatWatch = exports.copycatWatch = fileWatch;
 
